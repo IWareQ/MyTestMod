2036b66ea30a36471166e427eca353e66f3a1a67
Magn1tola <kurqwertyar@gmail.com>
2023-12-15 17:01:08 +0400

    block with mane icons

--- src/main/java/me/iwareq/mytestmod/block/BlockRegistry.java
+++ src/main/java/me/iwareq/mytestmod/block/BlockRegistry.java
@@ -3,17 +3,14 @@ package me.iwareq.mytestmod.block;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import me.iwareq.mytestmod.block.impl.TestBlock;
+import me.iwareq.mytestmod.block.impl.CraftTable;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 
 public class BlockRegistry {
 
-    public static final CreativeTabs TABS = new CreativeTabs(100, "testtab") {
-        @SuppressWarnings("unused")
-        private static final String __OBFID = "CL_00000100";
-
+    public static final CreativeTabs TABS = new CreativeTabs("test_tab") {
         @SideOnly(Side.CLIENT)
         public Item getTabIconItem() {
             return Item.getItemFromBlock(Blocks.obsidian);
@@ -21,6 +18,6 @@ public class BlockRegistry {
     };
 
     public static void init() {
-        GameRegistry.registerBlock(new TestBlock(), "testblock");
+        GameRegistry.registerBlock(new CraftTable(), "craft_table");
     }
 }
---
+++ src/main/java/me/iwareq/mytestmod/block/impl/CraftTable.java
@@ -0,0 +1,66 @@
+package me.iwareq.mytestmod.block.impl;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import me.iwareq.mytestmod.block.BlockRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class CraftTable extends Block {
+
+    private IIcon sideIcon;
+    private IIcon frontIcon;
+    private IIcon topIcon;
+
+    public CraftTable() {
+        super(Material.cake);
+        this.setBlockName("craft_table");
+        this.setCreativeTab(BlockRegistry.TABS);
+        this.setHardness(15F);
+        this.setResistance(10F);
+        this.setHarvestLevel("pickaxe", 3);
+        this.setLightLevel(15F);
+        this.setBlockTextureName("mytestmod:craftTableFront");
+    }
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        frontIcon = reg.registerIcon("mytestmod:craftTableFront");
+        sideIcon = reg.registerIcon("mytestmod:craftTableSide");
+        topIcon = reg.registerIcon("mytestmod:craftTableTop");
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void onBlockPlacedBy(World worldIn, int x, int y, int z, EntityLivingBase placer, ItemStack itemIn) {
+        super.onBlockPlacedBy(worldIn, x, y, z, placer, itemIn);
+
+        int dir = (MathHelper.floor_double((double)(placer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3);
+        int[] r = {2, 5, 3, 4};
+        worldIn.setBlockMetadataWithNotify(x, y, z, r[dir], 3);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(int side, int meta) {
+        ForgeDirection dir = ForgeDirection.getOrientation(side);
+        ForgeDirection blockDir;
+
+        if (meta != 0){
+            blockDir = ForgeDirection.getOrientation(meta);
+        }else {
+            blockDir = ForgeDirection.UP;
+        }
+
+        if (blockDir == dir) return frontIcon;
+        if (blockDir == ForgeDirection.UP || blockDir == ForgeDirection.DOWN) return topIcon;
+        return sideIcon;
+    }
+}
