351d4a010df01dbc52c4571c2f4234d29a3f3683
Dmitry Poyarkov <dimonpo96@mail.ru>
2024-01-25 21:34:52 +0400

    oops

--- src/main/java/me/iwareq/mytestmod/client/renderer/item/PortalGunRenderer.java
+++ src/main/java/me/iwareq/mytestmod/client/renderer/item/PortalGunRenderer.java
@@ -6,13 +6,13 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 
+import static me.iwareq.mytestmod.Resources.TEXTURE_PORTAL_GUN;
 import static me.iwareq.mytestmod.Resources.MODEL_PORTAL_GUN;
-import static me.iwareq.mytestmod.Resources.OBJ_MODEL_PORTAL_GUN;
 
 public class PortalGunRenderer extends CustomItemRenderer {
 
-    private static final ResourceLocation TEXTURE = Resources.of(MODEL_PORTAL_GUN);
-    private static final IModelCustom MODEL = AdvancedModelLoader.loadModel(Resources.of(OBJ_MODEL_PORTAL_GUN));
+    private static final ResourceLocation TEXTURE = Resources.of(TEXTURE_PORTAL_GUN);
+    private static final IModelCustom MODEL = AdvancedModelLoader.loadModel(Resources.of(MODEL_PORTAL_GUN));
 
     @Override
     public void bindTextures() {
